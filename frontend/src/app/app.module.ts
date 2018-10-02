3b945893db6867f741094975fbcf0df26a96f8ea
seven.wk <seven.wk@foxmail.com>
2018-10-02 14:27:22 +0800

    1.引入Layui框架文件 2.添加部分页面

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { ContentSectionComponent } from './content-section/content-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    ContentSectionComponent
   ],
   imports: [
     BrowserModule
