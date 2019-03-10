2d0bfd9129fe922fcf3f11eeb5ddedd0fe294462
3795 <seven.iwi@gmail.com>
2019-03-10 16:56:34 +0800

    删除之前的旧的模块

--- frontend/src/app/directive/editor-show/editor-show.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EditorShowDirective } from './editor-show.directive';
-
-describe('EditorShowDirective', () => {
-  it('should create an instance', () => {
-    const directive = new EditorShowDirective("3");
-    expect(directive).toBeTruthy();
-  });
-});
--- frontend/src/app/directive/editor-write/editor-write.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EditorWriteDirective } from './editor-write.directive';
-
-describe('EditorWriteDirective', () => {
-  it('should create an instance', () => {
-    const directive = new EditorWriteDirective("6");
-    expect(directive).toBeTruthy();
-  });
-});
