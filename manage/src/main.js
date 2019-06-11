fb8a021a8fd86046e5bd772e6329bfa499071c99
3795 <seven.iwi@gmail.com>
2019-06-11 18:10:36 +0800

    新增部分代码

--- manage/src/main.js
+++ manage/src/main.js
@@ -4,26 +4,15 @@ import router from './router'
 import store from './store'
 import iView from 'iview'
 import 'iview/dist/styles/iview.css'
-import 'mavon-editor/dist/css/index.css'
 import './assets/css/reset.css'
 import './assets/css/global.css'
 
-import mavonEditor from 'mavon-editor'
-Vue.config.productionTip = false;
 Vue.use(iView);
-Vue.use(mavonEditor);
 
-router.beforeEach((to, from, next) => {
-  iView.LoadingBar.start();
-  next();
-});
-
-router.afterEach(route => {
-  iView.LoadingBar.finish();
-});
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app');
+}).$mount('#app')
